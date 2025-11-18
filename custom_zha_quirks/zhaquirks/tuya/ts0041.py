d49c60d22ba333e17e3f9b511911f188ac69b453
cdavis@stoneydavis.com <cdavis@stoneydavis.com>
2025-11-17 21:16:24 -0800

    updated quirks

--- custom_zha_quirks/zhaquirks/tuya/ts0041.py
+++ custom_zha_quirks/zhaquirks/tuya/ts0041.py
@@ -2,7 +2,7 @@
 
 from zigpy.profiles import zha
 from zigpy.quirks import CustomDevice
-from zigpy.zcl.clusters.general import Basic, OnOff, Ota, PowerConfiguration, Time
+from zigpy.zcl.clusters.general import Basic, OnOff, Ota, PowerConfiguration, Time, Identify, Groups, PollControl
 
 from zhaquirks.const import (
     BUTTON_1,
@@ -237,3 +237,56 @@ class TuyaSmartRemote0041_var04(CustomDevice):
         (LONG_PRESS, BUTTON_1): {ENDPOINT_ID: 1, COMMAND: LONG_PRESS},
         (DOUBLE_PRESS, BUTTON_1): {ENDPOINT_ID: 1, COMMAND: DOUBLE_PRESS},
     }
+
+class TuyaSmartRemote0041Button(CustomDevice):
+    """Tuya 1-button remote device."""
+
+    signature = {
+        # SizePrefixedSimpleDescriptor(endpoint=1, profile=260, device_type=0, device_version=1, input_clusters=[0, 1, 3, 4, 20], output_clusters=[4, 6, 19])
+        MODEL: "TS0041",
+        ENDPOINTS: {
+            1: {
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: zha.DeviceType.ON_OFF_SWITCH,
+                INPUT_CLUSTERS: [
+                    Basic.cluster_id,
+                    PowerConfiguration.cluster_id,
+                    Identify.cluster_id,
+                    Groups.cluster_id,
+                    PollControl.cluster_id,
+                ],
+                OUTPUT_CLUSTERS: [
+                    Ota.cluster_id,
+                    OnOff.cluster_id,
+                    Groups.cluster_id
+                ],
+            },
+        },
+    }
+    replacement = {
+        ENDPOINTS: {
+            1: {
+                PROFILE_ID: zha.PROFILE_ID,
+                DEVICE_TYPE: zha.DeviceType.ON_OFF_SWITCH,
+                INPUT_CLUSTERS: [
+                    Basic.cluster_id,
+                    PowerConfiguration.cluster_id,
+                    Identify.cluster_id,
+                    Groups.cluster_id,
+                    PollControl.cluster_id,
+                ],
+                OUTPUT_CLUSTERS: [
+                    Ota.cluster_id,
+                    OnOff.cluster_id,
+                    Groups.cluster_id
+                ],
+            },
+        },
+    }
+
+    device_automation_triggers = {
+        (SHORT_PRESS, BUTTON_1): {ENDPOINT_ID: 1, COMMAND: SHORT_PRESS},
+        (LONG_PRESS, BUTTON_1): {ENDPOINT_ID: 1, COMMAND: LONG_PRESS},
+        (DOUBLE_PRESS, BUTTON_1): {ENDPOINT_ID: 1, COMMAND: DOUBLE_PRESS},
+    }
+
